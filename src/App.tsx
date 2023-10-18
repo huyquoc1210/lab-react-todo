49b3d8a869ddd66ad183de5534da92e266dd1ff1
huyquoc1210 <contact.huyquoc@gmail.com>
2023-10-18 22:39:37 +0700

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import { TodoApp } from "./components/TodoApp/";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./theme";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
